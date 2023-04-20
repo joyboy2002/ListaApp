274b96d2e9a4af03e0af3559b88fccec2c22627e
Emiliano Lopez <lopezemiliano195@gmail.com>
2023-04-19 19:14:21 -0600

    Initial Commit

---
+++ ListaApp/ListaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListaAppApp.swift
+//  ListaApp
+//
+//  Created by ISSC_611_2023 on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ListaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
